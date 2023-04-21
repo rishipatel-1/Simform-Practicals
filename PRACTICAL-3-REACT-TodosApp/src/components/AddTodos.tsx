7d28dff7d5fa47fc2aed000279bf5f1716649698
Rishi Patel <riishipatel.2.1@gmail.com>
2023-04-21 18:02:40 +0530

    News functionality added

--- PRACTICAL-3-REACT-TodosApp/src/components/AddTodos.tsx
+++ PRACTICAL-3-REACT-TodosApp/src/components/AddTodos.tsx
@@ -1,79 +1,95 @@
-import React, { useState, KeyboardEvent, ChangeEvent,FC } from 'react';
+import React, { useState, KeyboardEvent, ChangeEvent, FC, forwardRef, ForwardedRef, useImperativeHandle, useRef, useEffect } from 'react';
 import { toast } from 'react-toastify';
 
-
 interface Props {
-addTodo: (title: string) => void;
+  addTodo: (title: string) => void;
 }
 
-const AddTodos: FC<Props> = (props: Props) => {
-const [showAddTodos, setshowAddTodos] = useState<boolean>(true);
-const [curTitle, setCurTitle] = useState<string>('');
+interface AddTodosRef {
+  addTodoSubmithandler: (event: KeyboardEvent<HTMLInputElement>) => void;
+}
 
+const AddTodos: FC<Props> = forwardRef((props: Props, ref: ForwardedRef<AddTodosRef>) => {
+  const [showAddTodos, setshowAddTodos] = useState<boolean>(true);
+  const [curTitle, setCurTitle] = useState<string>('');
 
-const setTitle = (event: ChangeEvent<HTMLInputElement>) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const setTitle = (event: ChangeEvent<HTMLInputElement>) => {
     setCurTitle(event.target.value);
-};
+  };
 
-const addTodoSubmithandler = (event: KeyboardEvent<HTMLInputElement>) => {
+  const addTodoSubmithandler = (event: KeyboardEvent<HTMLInputElement>) => {
     event.preventDefault();
     if (!curTitle) {
-        toast.warn("Todo Title cannot be blank", {
-            autoClose: 3000
-        });
-        return;
+      toast.warn('Todo Title cannot be blank', {
+        autoClose: 3000,
+        position: toast.POSITION.TOP_CENTER,
+        className: 'custom-toast',
+      });
+      return;
     }
     props.addTodo(curTitle);
-    setshowAddTodos(prevState => !prevState);
-    toast.success("Todo Added Sucessfully", {
-        autoClose: 3000,
-        
+    setshowAddTodos(false);
+    setCurTitle('');
+    toast.success('Todo Added Successfully', {
+      autoClose: 3000,
+      position: toast.POSITION.TOP_CENTER,
+      className: 'custom-toast',
     });
-};
+  };
 
-
-const cancelTodosHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+  const cancelTodosHandler = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Escape') {
-        setshowAddTodos(prevState => !prevState);
+      setshowAddTodos((prevState) => !prevState);
     }
 
     if (event.key === 'Enter') {
-        addTodoSubmithandler(event);
-        setshowAddTodos(prevState => !prevState);
-       
+      addTodoSubmithandler(event);
+      setshowAddTodos((prevState) => !prevState);
     }
-};
+  };
 
-const keyPressHandler = () => {
-    setshowAddTodos(prevState => !prevState);
-};
+  const keyPressHandler = () => {
+    setshowAddTodos((prevState) => !prevState);
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
 
-return (
+  useImperativeHandle(ref, () => ({
+    addTodoSubmithandler,
+  }));
+
+  return (
     <div>
-        <form onSubmit={()=>addTodoSubmithandler}> 
-            {!showAddTodos && (
-                <div className='inputDiv' >
-                    <input
-                        id='title'  
-                        className='todoinput'
-                        type='text'
-                        placeholder='Add Your Todo'
-                        onKeyDown={cancelTodosHandler}
-                        onChange={setTitle}
-                        autoFocus
-                    />
-                </div>
-            )}
-        </form>
-        <div>
-            {showAddTodos && (
-                <div className='addTodos-main'>
-                    <button onClick={keyPressHandler} className='addTodos-plus'>+</button>
-                </div>
-            )}
-        </div>
+      <form onSubmit={()=>addTodoSubmithandler}>
+        {!showAddTodos && (
+          <div className="inputDiv">
+            <input
+              id="title"
+              className="todoinput"
+              type="text"
+              placeholder="Add Your Todo"
+              onKeyDown={cancelTodosHandler}
+              onChange={setTitle}
+              autoFocus
+              ref={inputRef}
+            />
+          </div>
+        )}
+      </form>
+      <div>
+        {showAddTodos && (
+          <div className="addTodos-main">
+            <button onClick={keyPressHandler} className="addTodos-plus">
+              +
+            </button>
+          </div>
+        )}
+      </div>
     </div>
-);
-};
+  );
+});
 
 export default AddTodos;
--- PRACTICAL-3-REACT-TodosApp/src/components/TodosList.tsx
+++ PRACTICAL-3-REACT-TodosApp/src/components/TodosList.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-
-
+import React, { useRef, useEffect, FC, forwardRef, ForwardedRef } from 'react';
+import { toast } from 'react-toastify';
 
 interface Todo {
   title: string;
@@ -15,56 +14,80 @@ interface Props {
   onDelete: (todo: Todo) => void;
 }
 
-const TodosList: React.FC<Props> = (props: Props) => {
-
+const TodosList: FC<Props> = forwardRef((props: Props, ref) => {
   const curDateObj = new Date();
   const curDate = curDateObj.getDate();
+  const lastTodoRef = useRef<HTMLDivElement>(null);
+  const checkboxRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (lastTodoRef.current && checkboxRef.current) {
+      lastTodoRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    if (inputRef.current) {
+      inputRef.current.addEventListener('keydown', handleInputKeyDown);
+    }
+
+    return () => {
+      if (inputRef.current) {
+        inputRef.current.removeEventListener('keydown', handleInputKeyDown);
+      }
+    };
+  }, [props.todosList]);
+
+  const handleInputKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      if (lastTodoRef.current) {
+        lastTodoRef.current.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  };
 
   return (
     <>
-      <div className="todolist-title"> <h2>Todos List</h2></div>
+      <div className="todolist-title">
+        <h2>Todos List</h2>
+      </div>
       <div className='todolist-main'>
-
         <div className='todolist'>
-          {props.todosList.length === 0 ? <span>No Todos to display</span>
-            : props.todosList.map((todo: Todo) => {
+          {props.todosList.length === 0 ? (
+            <span>No Todos to display</span>
+          ) : (
+            props.todosList.map((todo: Todo, index) => {
+              const completedStyle = todo.isTaskCompleted ? 'completedstyle' : '';
+              const isLastTodo = index === props.todosList.length - 1;
 
-
-              const completedStyle = todo.isTaskCompleted ? "completedstyle" : '';
-              const completedStyleCross = todo.isTaskCompleted ? 'todo-cross-green' : 'todo-cross';
               return (
-                <div className='todolist-item' key={todo.UniqueKey}>
-                  {
-                    <h3 className={completedStyle} >
-                      {todo.title}
-                    </h3>
-                  }
-
+                <div
+                  className={`todolist-item ${isLastTodo ? 'last-todo' : ''}`}
+                  key={todo.UniqueKey}
+                  ref={isLastTodo ? lastTodoRef : null}
+                >
+                  <div className='todo-title'>
+                    <h3 className={completedStyle}>{todo.title}</h3>
+                  </div>
                   <input
-                    type="checkbox"
+                    type='checkbox'
                     className='checkBox'
                     checked={todo.isTaskCompleted}
                     onChange={(e) => {
                       const updatedTodo = { ...todo, isTaskCompleted: e.target.checked };
                       props.onDelete(updatedTodo);
-
                     }}
+                    ref={checkboxRef}
+              
                   />
-                
-
-
-
-
                 </div>
-              )
+              );
             })
-          }
+          )}
         </div>
+     
       </div>
     </>
   );
-};
+});
 
 export default TodosList;
-
-
