5fc7626a5dda55c4aab6eb6106a8afea1b48c90d
Rishi Patel <riishipatel.2.1@gmail.com>
2023-05-15 14:30:33 +0530

    Signup,login and user page added

---
+++ React-Practical-6-LoginUserDetails/src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Form from "./components/LoginPage/Form/Form"
+import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Login from './components/LoginPage/Login/Login';
+import UserInfo from './components/UserInfo/UserInfo';
+import Header from './components/Header/Header';
+import Footer from "./components/Footer/Footer"
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App m-0 p-0">
+      <Header/>
+
+    <Routes>
+      <Route path="/" element={<Form />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/userinfo" element={<UserInfo />} />
+    </Routes>
+  
+      
+   <Footer/>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
